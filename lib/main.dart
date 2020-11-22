3a8337b9ca814b52a79e3d389748d4c5ce69f247
Aia Rupsom <aiale88@gmail.com>
2020-11-22 19:13:42 +0200

    using http package to make a network request

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 import 'package:world_time/pages/choose_location.dart';
 
-void main() => runApp(MaterialApp(initialRoute: '/home', routes: {
+void main() => runApp(MaterialApp(initialRoute: '/', routes: {
       '/': (context) => Loading(),
       '/home': (context) => Home(),
       '/location': (context) => ChooseLocation(),
