0f162f08a530827ec85fee1d26d397bb7287c3ed
Aia Rupsom <aiale88@gmail.com>
2020-11-23 09:53:46 +0200

    making a request to the world time api

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 import 'package:world_time/pages/choose_location.dart';
 
-void main() => runApp(MaterialApp(initialRoute: '/', routes: {
-      '/': (context) => Loading(),
-      '/home': (context) => Home(),
-      '/location': (context) => ChooseLocation(),
-    }));
+void main() => runApp(
+      MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Loading(),
+          '/home': (context) => Home(),
+          '/location': (context) => ChooseLocation(),
+        },
+      ),
+    );
