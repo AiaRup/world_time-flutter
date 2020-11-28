20aa978f0cad1675c8b5fd33075e01bcf014d8d5
Aia Rupsom <aiale88@gmail.com>
2020-11-28 14:29:56 +0200

    finish on press a country to send new data to home screen with updated time

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,13 +9,8 @@ class _HomeState extends State<Home> {
   Map data = {};
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context).settings.arguments;
+    data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
 
     // set background image
     String bgImage = data['isDaytime'] ? 'day.jpg' : 'night.jpg';
@@ -36,8 +31,19 @@ class _HomeState extends State<Home> {
             child: Column(
               children: [
                 FlatButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/location');
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    if (result != null) {
+                      setState(() {
+                        data = {
+                          'time': result['time'],
+                          'location': result['location'],
+                          'isDaytime': result['isDaytime'],
+                          'flag': result['flag']
+                        };
+                      });
+                    }
                   },
                   icon: Icon(
                     Icons.edit_location,
